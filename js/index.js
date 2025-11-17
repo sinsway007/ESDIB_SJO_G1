d30b6be53bef52421a58aaaabb9cc17f3a910e5a
erjoker <samuel.7acosta@gmail.com>
2025-11-17 19:29:39 +0100

    Añadido letra Anton al index.html y codigo javascript en index.js

---
+++ js/index.js
@@ -0,0 +1,11 @@
+const nav = document.querySelector("#");
+const abrir = document-querySelector("#abrir");
+const cerrar = document.querySelector("#cerrar");
+
+abrir.addEventListner("click", () =>{
+    nav.classList.add("visible");
+})
+
+cerrar.addEventListener("click", () =>{
+    nav.classList.remove("visible")
+})
